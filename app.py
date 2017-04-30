f6f086ec4002dce0ff076b7fae58b2b5965f13f1
Aleph Melo <alephmelo@icloud.com>
2017-04-30 00:50:11 -0300

    Add more options.

--- app.py
+++ app.py
@@ -11,12 +11,18 @@ SETTINGS = get_settings()
 request = requests.get(APIS.get('fixer', '').format(SETTINGS['currency']['base']))
 response = request.json()
 
+
+currency_text = str()
+
+for currency_to_show in SETTINGS['currency']['to_show']:
+	currency_text += """{0}: {1}\n""".format(currency_to_show, response['rates'][currency_to_show])
+
 notification = {
     'header': 'Dollar Now',
-    'text': """R$ {}
-            """.format(response['rates']['BRL'])
+    'text': currency_text
 }
 
 n = notify2.Notification(notification['header'], notification['text'])
 n.show()
 n.set_timeout(100)
+raw_input()
